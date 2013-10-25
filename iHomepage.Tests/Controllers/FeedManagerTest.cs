3caae45702265d6ee27b31529822e63c049b3361
Alexander Wicht <alexander.wicht@mifa.de>
2013-10-25 12:32:02 +0200

    Added Feedmanager.GetAllFeeds()

---
+++ iHomepage.Tests/Controllers/FeedManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using iHomepage;
+using iHomepage.Models;
+using System.ServiceModel.Syndication;
+
+namespace iHomepage.Tests.Controllers
+{
+    [TestClass]
+    public class FeedManagerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+
+        }
+
+    }
+}
--- iHomepage/Models/FeedManager.cs
+++ iHomepage/Models/FeedManager.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Web;
+using System.Xml;
 
 namespace iHomepage.Models
 {
     public class FeedManager
     {
+
+        iHomepageEntities context;
+
+        public FeedManager(iHomepageEntities context)
+        {
+            this.context = context;
+        }
+
         public List<SyndicationFeed> GetAllFeeds()
         {
-            throw new NotImplementedException();
+            var dbFeeds = context.Feeds.ToList();
+
+            List<SyndicationFeed> feeds = new List<SyndicationFeed>();
+
+            foreach (var item in dbFeeds)
+            {
+                XmlReader xmlfeed = XmlReader.Create(item.Uri);
+
+                feeds.Add(SyndicationFeed.Load(xmlfeed));
+            }            
+
+            return feeds;
         }
     }
 }
