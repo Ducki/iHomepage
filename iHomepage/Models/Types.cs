d08ce7b638fb433e4139b6f927bd2e2187db0196
Alexander Wicht <alexander.wicht@mifa.de>
2013-10-25 13:51:05 +0200

    Added type ConfiguredJsonFeed

--- iHomepage/Models/Types.cs
+++ iHomepage/Models/Types.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel.Syndication;
 
 namespace iHomepage.Models
 {
     internal class ConfiguredJsonFeed
     {
+        public List<SyndicationItem> FeedItems { get; set; }
+        public int DisplayColumn { get; set; }
+        public int DisplayRow { get; set; }
     }
+
+
 }
