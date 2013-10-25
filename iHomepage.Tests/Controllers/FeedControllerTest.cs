3601e4384f1a5bde86dfcb4dd045940b3e85a855
Alexander Wicht <alexander.wicht@mifa.de>
2013-10-25 14:12:29 +0200

    Implemented FeedControllerTest.Index()

---
+++ iHomepage.Tests/Controllers/FeedControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using iHomepage;
+using iHomepage.Controllers;
+
+namespace iHomepage.Tests.Controllers
+{
+    [TestClass]
+    public class FeedControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            FeedController controller = new FeedController();
+
+            // Act
+            JsonResult result = controller.GetAllConfiguredFeeds() as JsonResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
