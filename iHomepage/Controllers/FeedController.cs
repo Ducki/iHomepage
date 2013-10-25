b7102f9eabeb2989267c7d7e061c371eb529ff2e
Alexander Wicht <alexander.wicht@mifa.de>
2013-10-25 14:11:24 +0200

    Added Controllers.GetAllConfiguredFeeds()

--- iHomepage/Controllers/FeedController.cs
+++ iHomepage/Controllers/FeedController.cs
@@ -5,12 +5,17 @@ using System.Web;
 using System.Web.Mvc;
 using System.ServiceModel.Syndication;
 using System.Xml;
+using iHomepage.Models;
+using System.Web.Helpers;
 
 
 namespace iHomepage.Controllers
 {
     public class FeedController : Controller
     {
+
+        iHomepage.Models.iHomepageEntities context = new Models.iHomepageEntities();
+
         //
         // GET: /Feed/
         public ActionResult Index()
@@ -22,6 +27,15 @@ namespace iHomepage.Controllers
         public JsonResult GetAllFeeds()
         {
             throw new NotImplementedException();
+        }
+
+        public JsonResult GetAllConfiguredFeeds()
+        {
+            FeedManager fm = new FeedManager(context);
+
+            List<ConfiguredFeed> feeds = fm.GetConfiguredFeeds();
+
+            return Json(feeds, JsonRequestBehavior.AllowGet);
         }
 	}
 }
