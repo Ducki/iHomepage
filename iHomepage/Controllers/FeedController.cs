357d2f45f014433345f2f5fce658c9f7b2d744db
Alexander Wicht <alexander.wicht@mifa.de>
2013-10-25 11:37:47 +0200

    Added Feedmanager/controller scaffold

---
+++ iHomepage/Controllers/FeedController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+
+namespace iHomepage.Controllers
+{
+    public class FeedController : Controller
+    {
+        //
+        // GET: /Feed/
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+
+        public JsonResult GetAllFeeds()
+        {
+            throw new NotImplementedException();
+        }
+	}
+}
---
+++ iHomepage/Models/FeedManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+
+namespace iHomepage.Models
+{
+    public class FeedManager
+    {
+        public List<SyndicationFeed> GetAllFeeds()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
