e3f0193904e8af81a62dca38db9510d72688a135
Alexander Wicht <alexwichti@live.de>
2013-10-26 18:25:03 +0200

    Refactored FeedManager.js to use backbone.sync

--- iHomepage/Scripts/FeedManager.js
+++ iHomepage/Scripts/FeedManager.js
@@ -3,20 +3,22 @@
 /// <reference path="http://documentcloud.github.com/underscore/underscore-min.js" />
 /// <reference path="http://documentcloud.github.com/backbone/backbone-min.js" />
 
-var FeedListModel = Backbone.Model.extend({
+App = {};
+
+App.FeedListModel = Backbone.Model.extend({
     defaults: {
-        FeedItems: "",
+        Feed: "",
         DisplayColumn: "",
-        DisplayRow: "",
-        Title: ""
+        DisplayRow: ""
     }
 });
 
-var FeedListCollection = Backbone.Collection.extend({
-    model: FeedListModel
+App.FeedListCollection = Backbone.Collection.extend({
+    model: App.FeedListModel,
+    url: "/Feed/GetAllConfiguredFeeds"
 });
 
-var FeedItemModel = Backbone.Model.extend({
+App.FeedItemModel = Backbone.Model.extend({
     defaults: {
         Title: "",
         Link: "",
@@ -24,33 +26,26 @@ var FeedItemModel = Backbone.Model.extend({
     }
 });
 
-var FeedCollection = Backbone.Collection.extend({
-    model: FeedItemModel
+App.FeedCollection = Backbone.Collection.extend({
+    model: App.FeedItemModel
 });
 
-var FeedManager = Backbone.Model.extend({
+App.FeedManager = Backbone.Model.extend({
 
-    FetchFeedsList: function () {
-        var self = this;
-
-        $.getJSON("/Feed/GetAllConfiguredFeeds", null, function (data, textStatus, jqXHR) {
-            console.log(data);
-        });
+    initialize: function (feedListCollection) {
+        this.feedListCollection = feedListCollection;
+        this.feedListCollection.fetch();
     },
 
-    FetchSingleFeed: function () {
-        // fetch xml from uri, extract title, description and url
-    },
-
-    CreateFeedsCollection: function () {
-        // Loop through FeedListCollection and call FetchSingleFeed for every entry.
-        // Combine in FeedsCollection
-    }
 });
 
-var AppView = Backbone.View.extend({
+App.AppView = Backbone.View.extend({
 
     initialize: function () {
+
+        this.feedListCollection = new App.FeedListCollection();
+
+        this.fm = new App.FeedManager(this.feedListCollection);
 
     },
 
@@ -58,68 +53,3 @@ var AppView = Backbone.View.extend({
 
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// snippet:
-
-function get_rss_feed() {
-    //clear the content in the div for the next feed.
-    $("#feedContent").empty();
-
-    //use the JQuery get to grab the URL from the selected item, put the results in to an argument for parsing in the inline function called when the feed retrieval is complete
-    $.get($('#diffFeeds').val(), function (d) {
-
-        //find each 'item' in the file and parse it
-        $(d).find('item').each(function () {
-
-            //name the current found item this for this particular loop run
-            var $item = $(this);
-            // grab the post title
-            var title = $item.find('title').text();
-            // grab the post's URL
-            var link = $item.find('link').text();
-            // next, the description
-            var description = $item.find('description').text();
-            //don't forget the pubdate
-            var pubDate = $item.find('pubDate').text();
-
-            // now create a var 'html' to store the markup we're using to output the feed to the browser window
-            var html = "<div class=\"entry\"><h2 class=\"postTitle\">" + title + "<\/h2>";
-            html += "<em class=\"date\">" + pubDate + "</em>";
-            html += "<p class=\"description\">" + description + "</p>";
-            html += "<a href=\"" + link + "\" target=\"_blank\">Read More >><\/a><\/div>";
-
-            //put that feed content on the screen!
-            $('#feedContent').append($(html));
-        });
-    });
-
-};
-
-    
